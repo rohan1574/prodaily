4c144cd8d2e9f1b6cb467112ba21df5db12b69f3
Rony <mrrohan192@gmail.com>
2025-03-09 17:13:07 +0600

    add:the tailwind css setup

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { s as tw } from 'react-native-wind';
 export default function App() {
   return (
     <View style={tw`flex-1 justify-center items-center bg-gray-100`}>
-      <Text style={tw`text-xl font-bold text-gray-800`}>Hello, NativeWind!</Text>
+      <Text style={tw`text-xl font-bold text-red-500`}>Hello, NativeWind!</Text>
     </View>
   );
 }
