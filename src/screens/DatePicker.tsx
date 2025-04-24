94bc8677870b281bab5b5fb18cc5dc0575a8c180
Rony <mrrohan192@gmail.com>
2025-04-24 07:49:03 +0600

    add:the added design

--- src/screens/DatePicker.tsx
+++ src/screens/DatePicker.tsx
@@ -24,24 +24,37 @@ const DatePicker: React.FC<DatePickerProps> = ({
 
   return (
     <Modal transparent={true} visible={true}>
-      <View style={tw`flex-1 justify-center items-center bg-gray-500 bg-opacity-50`}>
+      <View style={tw`flex-1 justify-center items-center bg-opacity-50 top-16`}>
         <View style={tw`bg-white p-4 rounded-lg w-80`}>
           <Text style={tw`text-lg font-bold mb-4`}>Select Dates</Text>
           <FlatList
-            data={Array.from({ length: 31 }, (_, i) => i + 1)} // 1 to 31
+            data={Array.from({ length: 31 }, (_, i) => i + 1)}
+            numColumns={6}
             renderItem={({ item }) => (
-              <TouchableOpacity
-                onPress={() => toggleDateSelection(item)}
-                style={[
-                  tw`py-2 px-4 rounded-md mb-2`,
-                  selectedDate.includes(item) ? tw`bg-blue-500` : tw`bg-gray-200`,
-                ]}>
-                <Text style={tw`text-center`}>{item}</Text>
-              </TouchableOpacity>
+              <View style={tw`flex-1 mx-1 mb-1`}>
+                <TouchableOpacity
+                  onPress={() => toggleDateSelection(item)}
+                  style={[
+                    tw`rounded-md aspect-square justify-center`,
+                    selectedDate.includes(item) 
+                      ? tw`bg-blue-500` 
+                      : tw`bg-gray-200`,
+                  ]}>
+                  <Text 
+                    style={[
+                      tw`text-center text-sm`,
+                      selectedDate.includes(item) && tw`text-white`
+                    ]}
+                  >
+                    {item}
+                  </Text>
+                </TouchableOpacity>
+              </View>
             )}
             keyExtractor={(item) => item.toString()}
+            contentContainerStyle={tw`p-1`}
           />
-          <View style={tw`flex-row justify-between mb-12`}>
+          <View style={tw`flex-row justify-between mt-4`}>
             <TouchableOpacity onPress={onCancel} style={tw`bg-gray-300 px-4 py-2 rounded-md`}>
               <Text>Cancel</Text>
             </TouchableOpacity>
