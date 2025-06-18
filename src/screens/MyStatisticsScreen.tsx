1044d832f360614323375743012624cf0935bba7
Rony <mrrohan192@gmail.com>
2025-06-18 21:51:34 +0600

    add:the added that themes color

--- src/screens/MyStatisticsScreen.tsx
+++ src/screens/MyStatisticsScreen.tsx
@@ -204,7 +204,7 @@ const MyStatisticsScreen = () => {
               tw`font-bold`,
               {fontSize: 20, letterSpacing: 1, },
             ]}>
-            My Statistics
+            My Statistic
           </Text>
           <Text
             style={[
